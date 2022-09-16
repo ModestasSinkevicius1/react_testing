358f4fa08e1bfae35e0937b513a7322780a338a8
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-09-16 14:48:31 +0300

    added integration test

---
+++ cypress/e2e/3-home-page/home.cy.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+
+describe('empty spec', () => {
+  beforeEach('passes', () => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Text show right display', () =>{
+    cy.get('.home').contains('Hello');
+  })
+
+  it('Empty input outputs no text', () =>{
+    const newItem = '';
+
+    cy.get('.btn').type(`${newItem}{enter}`);
+
+    const actual = cy.get('span').contains('rytas');
+
+    // eslint-disable-next-line no-unused-expressions
+    expect(actual).to.be.null;
+  })
+})
